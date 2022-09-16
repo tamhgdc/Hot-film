45394193fe6f922f7f0b6820fca3f30e6475c273
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-09-16 16:44:54 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withAnalyzer = require("@next/bundle-analyzer")({
+  enabled:
+    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "development",
+});
+const { withSuperjson } = require("next-superjson");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  output: "standalone",
+};
+
+module.exports = withSuperjson()(withAnalyzer(nextConfig));
